2aac1ab832e50982092f316e8fc3d12ba7050b96
“Young <hogusong1@gmail.com>
2019-03-10 16:15:00 -0400

    complete 'login' task: connected client-server-mongoose-server-client

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,31 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/providers/auth.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   pattern = "[a-zA-Z0-9]*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?";
   message = '';
+  loginSubscription = new Subscription();
 
   constructor(private authService: AuthService,
               private router: Router) { }
 
   ngOnInit() {
+    this.loginSubscription = this.authService.getLoginSubject()
+      .subscribe(res => {
+        if (res === 'LOGGEDIN') {
+          // this.router.navigate([''])
+        }
+        this.message = res;
+      })
   }
 
   onLogin(form: NgForm) {
     this.message = '';
     if (form.invalid) return;
-    const result = this.authService.login(form.value.email, form.value.password);
-    if (result.loggedIn) {
-      console.log('Logged in successfully!!!');
-    } 
-    this.message = result.message;
+    this.authService.login(form.value.email, form.value.password);
+  }
+
+  ngOnDestroy() {
+    this.loginSubscription.unsubscribe();
   }
 }
--- src/app/providers/auth.service.ts
+++ src/app/providers/auth.service.ts
@@ -2,27 +2,67 @@ import { Injectable } from '@angular/core';
 
 import { USER } from '../models'
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private token: string;
   private users: USER[] = [];
+  private loginSubject = new Subject<string>();
+  tokenTimer: any;
 
   constructor(private http: HttpClient) { }
+
+  getLoginSubject() {
+    return this.loginSubject.asObservable();
+  }
+
+  getToken() {
+    return this.token;
+  }
 
   signup(newUser: USER) {
     return this.http.post<any>('http://localhost:3000/api/signup', newUser)
   }
 
   login(email, password) {
-    const user = this.users.find(user => user.email === email);
-    if (!user) {
-      return { loggedIn: false, message: 'Failed to find the email. Confirm the email . . .' }
-    } else if (user.password != password) {
-      return { loggedIn: false, message: 'Password not matched. Try another . . .' }
-    } 
-    return { loggedIn: true, message: 'Logged in successfully. Enjoy my app !!!' };
+    this.http.post<{token:string, expiresIn: number, userId: string}>('http://localhost:3000/api/login', { email, password })
+      .subscribe(res => {
+        this.token = res.token;
+        const expiresIn = res.expiresIn * 1000
+        if (this.token) {
+          this.loginSubject.next('LOGGEDIN');
+          const expiration = (new Date()).getTime() + expiresIn;
+          this.saveAuthData(this.token, new Date(expiration), res.userId);
+          this.setAuthTimer(expiresIn);
+        }
+      }, error => {
+        this.loginSubject.next(error.error.message);
+      });
+  }
+
+  setAuthTimer(duration: number) {
+    this.tokenTimer = setTimeout(() => { this.logout() }, duration)
+  }
+
+  saveAuthData(token: string, expirationDate: Date, userId: string) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+    localStorage.setItem('userId', userId);
+  }
+
+  clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
+    localStorage.removeItem('userId');
+  }
+
+  logout() {
+    this.token = null;
+    clearTimeout(this.tokenTimer);
+    this.clearAuthData();
   }
 }
