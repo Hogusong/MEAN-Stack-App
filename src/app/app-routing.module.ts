3180268bd0bed48cd1947d1ef655c69cbd3190c3
“Young <hogusong1@gmail.com>
2019-03-13 21:22:04 -0400

    create AuthGuard to protect routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { PostListComponent } from "./posts/post-list/post-list.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { AuthGuard } from "./auth/auth-guard";
 
 const appRoutes: Routes = [
   { path: '', component: PostListComponent },
-  { path: 'create', component: PostCreateComponent },
-  { path: 'edit/:id', component: PostCreateComponent },
+  { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
+  { path: 'edit/:id', component: PostCreateComponent, canActivate: [AuthGuard] },
   { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
   { path: '**', redirectTo: '/auth/login' }
 ]
 
 @NgModule({
-  imports: [ RouterModule.forRoot(appRoutes)],
-  exports: [ RouterModule ]
+  imports: [ RouterModule.forRoot(appRoutes) ],
+  exports: [ RouterModule ],
+  providers: [ AuthGuard ]
 })
 export class AppRoutingModule {}
